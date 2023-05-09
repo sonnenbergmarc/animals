74c8077ca570d3091a50c842e8995933157784a9
sonnenbergmarc <msonnenberg@live.com>
2023-05-09 15:49:40 -0600

    Completed animals

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState} from 'react';
+import AnimalShow from './AnimalShow';
+
+function getRandomAnimal() {
+    const animals = ['bird', 'cat', 'cow', 'dog', 'gator', 'horse'];
+
+    return animals[Math.floor(Math.random() * animals.length)];
+}
+
+// console.log(getRandomAnimal());
+
+// function App() {
+//     const [animals, setAnimals] = useState([]);
+
+//     const handleClick = () => {
+//         setAnimals([...animals, getRandomAnimal()])
+//     };
+
+//     return (
+//     <div>
+//         <button onClick = {handleClick}>Add Animal</button>
+//         <div>{animals}</div>
+//     </div>
+//     );
+// }
+
+// import { useState} from 'react';
+
+// function getRandomAnimal() {
+//     const animals = ['bird', 'cat', 'cow', 'dog', 'gator', 'horse'];
+
+//     return animals[Math.floor(Math.random() * animals.length)];
+// }
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    const [animals, setAnimals] = useState([]);
+
+    const handleClick = () => {
+        // ['cat']
+
+        // ['cat', 'horse']
+        setAnimals([...animals, getRandomAnimal()])
+    };
+
+    const renderedAnimals = animals.map((animal, index) => {
+        return <AnimalShow type={animal} key={index} />
+    });
+
+    // const minusHandleClick = () => {
+    //     if (count > 0) {
+    //         setCount(count - 1);
+    //     }
+    // }
+
+    return (
+    <div className="app">
+        <button onClick = {handleClick}>Add Animal</button>
+        <div className="animal-list">{renderedAnimals}</div>
     </div>
-  );
+    );
 }
 
 export default App;
